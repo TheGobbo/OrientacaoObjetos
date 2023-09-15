449fccd395b514ff812af64d4ee7b6e851b80037
Gobbedu <gobbolegal@gmail.com>
2023-09-15 14:56:22 -0300

    aula 11 wip

---
+++ 11aula/ConteudoMinistrado.cpp
@@ -0,0 +1,20 @@
+#include "ConteudoMinistrado.hpp"
+
+unsigned int ConteudoMinistrado::proxId{0};
+
+ConteudoMinistrado::ConteudoMinistrado(std::string descricao, unsigned short cargaHorariaConteudo):
+    descricao(descricao), cargaHorariaConteudo(cargaHorariaConteudo), id{ConteudoMinistrado::proxId}{
+        ConteudoMinistrado::proxId++;
+}
+
+const std::string& ConteudoMinistrado::getDescricao(){
+	return descricao;
+}
+
+unsigned short ConteudoMinistrado::getCargaHorariaConteudo()const{
+	return cargaHorariaConteudo;
+}
+
+unsigned int ConteudoMinistrado::getId()const{
+	return id;
+}
